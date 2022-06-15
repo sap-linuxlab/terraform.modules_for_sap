5e6c2697a062177bda939d031672e2806e3d9063
sean-freeman <sean.freeman@outlook.com>
2022-06-15 14:36:40 +0100

    feat: new module test for proxy access

---
+++ ibmcloud_vs/host_network_access_sap_public_via_proxy/module_variables.tf
@@ -0,0 +1,16 @@
+
+variable "module_var_ibmcloud_vpc_subnet_name" {}
+
+variable "module_var_bastion_security_group_id" {}
+
+variable "module_var_bastion_connection_security_group_id" {}
+
+variable "module_var_host_security_group_id" {}
+
+variable "module_var_sap_nwas_pas_instance_no" {
+  default = "01"
+}
+
+variable "module_var_sap_hana_instance_no" {
+  default = "00"
+}
