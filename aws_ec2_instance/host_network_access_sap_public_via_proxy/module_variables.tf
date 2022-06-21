d2ea2801459e01912c9d2a418710a342198cb598
sean-freeman <sean.freeman@outlook.com>
2022-06-21 21:28:29 +0100

    feat: add proxy connection for aws

---
+++ aws_ec2_instance/host_network_access_sap_public_via_proxy/module_variables.tf
@@ -0,0 +1,15 @@
+variable "module_var_resource_prefix" {}
+
+variable "module_var_aws_vpc_subnet_id" {}
+
+variable "module_var_sap_nwas_pas_instance_no" {
+  default = "01"
+}
+
+variable "module_var_sap_hana_instance_no" {
+  default = "00"
+}
+
+variable "module_var_bastion_sg_id" {}
+
+variable "module_var_bastion_connection_sg_id" {}
