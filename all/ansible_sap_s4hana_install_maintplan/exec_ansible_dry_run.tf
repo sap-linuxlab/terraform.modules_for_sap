6ad0a848610ca8a07d4eaf401ecfd109760e673f
sean-freeman <sean.freeman@outlook.com>
2022-06-22 18:41:12 +0100

    fix: dry run for Ansible 2.9

--- all/ansible_sap_s4hana_install_maintplan/exec_ansible_dry_run.tf
+++ all/ansible_sap_s4hana_install_maintplan/exec_ansible_dry_run.tf
@@ -12,22 +12,27 @@ resource "null_resource" "ansible_exec_dry_run" {
     export GIT_CONFIG_GLOBAL=/dev/null
     export GIT_CONFIG_SYSTEM=/dev/null
 
-    ansible-galaxy collection install git+https://github.com/sap-linuxlab/community.sap_launchpad.git,main --collections-path ${path.root}/tmp/${var.module_var_hostname}/collections
-
-    # Must export the Shell Variable, otherwise it cannot be read by Ansible binaries
-    export ANSIBLE_COLLECTIONS_PATH="${abspath(path.root)}/tmp/${var.module_var_hostname}/collections"
-    #export ANSIBLE_ROLES_PATH="${abspath(path.root)}/tmp/${var.module_var_hostname}/roles"
-
     # Ansible Config - Forces color mode when run without a TTY
     export ANSIBLE_FORCE_COLOR=1
 
-    ansible_version="$(ansible --version | awk 'NR==1{print $3}' | sed 's/]//g')"
+    ansible_2_9_version="$(ansible --version | awk 'NR==1{print $2}' | sed 's/]//g')"
+    ansible_core_version="$(ansible --version | awk 'NR==1{print $3}' | sed 's/]//g')"
 
     # Simple resolution to version comparison: https://stackoverflow.com/a/37939589/8412427 . Added search_string variable and removed function to work around Windows using WSL2 and Ubuntu
     search_string="%d%03d%03d%03d\n"
-    if [ $(echo $ansible_version | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') -gt $(echo "2.9.0" | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') ] && [ $(echo $ansible_version | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') -lt $(echo "2.11.0" | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') ]; then
-        echo "Lower Ansible version than tested, may produce unexpected results"
-        export ANSIBLE_COLLECTIONS_PATHS="${abspath(path.root)}/tmp/${var.module_var_hostname}/collections"
+    if [ $(echo $ansible_2_9_version | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') -gt $(echo "2.9.0" | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') ] && [ $(echo $ansible_2_9_version | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') -lt $(echo "2.11.0" | awk -F '.' '{ printf "'$search_string'", $1,$2,$3,$4; }') ]; then
+      echo "Ansible $ansible_2_9_version detected"
+      echo "Lower Ansible version than tested, may produce unexpected results"
+      curl -L https://github.com/sap-linuxlab/community.sap_launchpad/archive/refs/heads/main.tar.gz -o ${path.root}/tmp/${var.module_var_hostname}/sap_launchpad-main.tar.gz
+      mkdir -p ${path.root}/tmp/${var.module_var_hostname}/ansible_collections/community/sap_launchpad
+      tar xvf ${path.root}/tmp/${var.module_var_hostname}/sap_launchpad-main.tar.gz --strip-components=1 -C ${path.root}/tmp/${var.module_var_hostname}/ansible_collections/community/sap_launchpad
+      # Must export the Shell Variable, otherwise it cannot be read by Ansible binaries
+      export ANSIBLE_COLLECTIONS_PATHS="${abspath(path.root)}/tmp/${var.module_var_hostname}"
+    else
+      ansible-galaxy collection install git+https://github.com/sap-linuxlab/community.sap_launchpad.git,main --collections-path ${path.root}/tmp/${var.module_var_hostname}
+      # Must export the Shell Variable, otherwise it cannot be read by Ansible binaries
+      export ANSIBLE_COLLECTIONS_PATH="${abspath(path.root)}/tmp/${var.module_var_hostname}"
+      #export ANSIBLE_ROLES_PATH="${abspath(path.root)}/tmp/${var.module_var_hostname}/roles"
     fi
 
     # Find Python used by Ansible on the localhost
