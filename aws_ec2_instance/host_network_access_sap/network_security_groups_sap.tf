4cf3ef55a57ab001de97f44d5d69529e6bfe3d36
sean-freeman <sean.freeman@outlook.com>
2022-09-29 00:45:49 +0100

    fix: aws sg wrong argument

--- aws_ec2_instance/host_network_access_sap/network_security_groups_sap.tf
+++ aws_ec2_instance/host_network_access_sap/network_security_groups_sap.tf
@@ -7,7 +7,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_sapnwas_sapgui" {
   from_port         = tonumber("32${var.module_var_sap_nwas_pas_instance_no}")
   to_port           = tonumber("32${var.module_var_sap_nwas_pas_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP NetWeaver PAS Gateway, access from within the same Subnet
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_sapnwas_gw" {
   from_port         = tonumber("33${var.module_var_sap_nwas_pas_instance_no}")
   to_port           = tonumber("33${var.module_var_sap_nwas_pas_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP HANA ICM HTTPS (Secure) Internal Web Dispatcher, access from within the same Subnet
@@ -29,7 +29,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_saphana_icm_https" {
   from_port         = tonumber("43${var.module_var_sap_hana_instance_no}")
   to_port           = tonumber("43${var.module_var_sap_hana_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP HANA ICM HTTP Internal Web Dispatcher, access from within the same Subnet
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_saphana_icm_http" {
   from_port         = tonumber("80${var.module_var_sap_hana_instance_no}")
   to_port           = tonumber("80${var.module_var_sap_hana_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP NetWeaver AS JAVA Message Server, access from within the same Subnet
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_sapnwas_java_ms" {
   from_port         = tonumber("81${var.module_var_sap_nwas_pas_instance_no}")
   to_port           = tonumber("81${var.module_var_sap_nwas_pas_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP HANA Internal Web Dispatcher, access from within the same Subnet
@@ -62,7 +62,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_saphana_webdisp" {
   from_port         = tonumber("3${var.module_var_sap_hana_instance_no}06")
   to_port           = tonumber("3${var.module_var_sap_hana_instance_no}06")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP HANA indexserver MDC System Tenant SYSDB, access from within the same Subnet
@@ -73,7 +73,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_saphana_index_mdc_sy
   from_port         = tonumber("3${var.module_var_sap_hana_instance_no}13")
   to_port           = tonumber("3${var.module_var_sap_hana_instance_no}13")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP HANA indexserver MDC Tenant #1, access from within the same Subnet
@@ -84,7 +84,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_tcp_ingress_saphana_index_mdc_1"
   from_port         = tonumber("3${var.module_var_sap_hana_instance_no}15")
   to_port           = tonumber("3${var.module_var_sap_hana_instance_no}15")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP Web GUI and SAP Fiori Launchpad (HTTPS), access from within the same Subnet
@@ -95,7 +95,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_sapfiori" {
   from_port         = tonumber("443${var.module_var_sap_hana_instance_no}")
   to_port           = tonumber("443${var.module_var_sap_hana_instance_no}")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 # SAP NetWeaver sapctrl HTTP and HTTPS, access from within the same Subnet
@@ -106,7 +106,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_sapnwas_ctrl" {
   from_port         = tonumber("5${var.module_var_sap_nwas_pas_instance_no}13")
   to_port           = tonumber("5${var.module_var_sap_nwas_pas_instance_no}14")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 
@@ -120,7 +120,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_saphana_hsr1" {
   from_port         = tonumber("4${var.module_var_sap_hana_instance_no}01")
   to_port           = tonumber("4${var.module_var_sap_hana_instance_no}07")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_saphana_hsr1" {
@@ -130,7 +130,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_saphana_hsr1" {
   from_port         = tonumber("4${var.module_var_sap_hana_instance_no}01")
   to_port           = tonumber("4${var.module_var_sap_hana_instance_no}07")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_saphana_hsr2" {
@@ -140,7 +140,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_saphana_hsr2" {
   from_port         = tonumber("4${var.module_var_sap_hana_instance_no}40")
   to_port           = tonumber("4${var.module_var_sap_hana_instance_no}40")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_saphana_hsr2" {
@@ -150,7 +150,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_saphana_hsr2" {
   from_port         = tonumber("4${var.module_var_sap_hana_instance_no}40")
   to_port           = tonumber("4${var.module_var_sap_hana_instance_no}40")
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_1" {
@@ -160,7 +160,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_1" {
   from_port         = 2224
   to_port           = 2224
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_1" {
@@ -170,7 +170,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_1" {
   from_port         = 2224
   to_port           = 2224
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_2" {
@@ -180,7 +180,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_2" {
   from_port         = 3121
   to_port           = 3121
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_2" {
@@ -190,7 +190,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_2" {
   from_port         = 3121
   to_port           = 3121
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_3" {
@@ -200,7 +200,7 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_ingress_pacemaker_3" {
   from_port         = 5404
   to_port           = 5412
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
 
 resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_3" {
@@ -210,5 +210,5 @@ resource "aws_security_group_rule" "vpc_sg_rule_sap_egress_pacemaker_3" {
   from_port         = 5404
   to_port           = 5412
   protocol          = "tcp"
-  ipv6_cidr_blocks  = [local.target_subnet_ip_range]
+  cidr_blocks  = [local.target_subnet_ip_range]
 }
