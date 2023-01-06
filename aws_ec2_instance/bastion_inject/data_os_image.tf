9f3d5e6e9412e2cae47cf2ff5e30997963a439dc
sean-freeman <sean.freeman@outlook.com>
2023-01-06 16:32:46 +0000

    fix: notes on aws os image changes

--- aws_ec2_instance/bastion_inject/data_os_image.tf
+++ aws_ec2_instance/bastion_inject/data_os_image.tf
@@ -2,12 +2,23 @@
 # Detect AMI
 # The AMI for the OS, changes per AWS Region
 
-# From command line look for RHEL 8.x images in the Region (defined in aws configure)
-# aws ec2 describe-images --filters 'Name=name,Values=*SAP-7.6*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
-# aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-7.7*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
-# aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-7.9*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
-# aws ec2 describe-images --filters 'Name=name,Values=*RHEL-8.1.0-SAP*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
-# aws ec2 describe-images --filters 'Name=name,Values=*RHEL-8.2.0-SAP*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+# From command line look for RHEL 8.x images in the Region (defined in aws configure). Search is case-sensitive
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-7.7*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-7.9*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-8.1*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-8.2*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-8.4*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-SAP-8.6*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*RHEL-8.6*_HVM*x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+
+# From command line look for SLES 15.x images in the Region (defined in aws configure). Search is case-sensitive
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-sap-12-sp5-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-sap-15-sp1-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-sap-15-sp2-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-sap-15-sp3-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-sap-15-sp4-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+#aws ec2 describe-images --filters 'Name=name,Values=*suse-sles-15-sp4-v202*-hvm-ssd-x86_64*' 'Name=architecture,Values=x86_64' | jq '.Images[] | .Name, .ImageId'
+
 
 data "aws_ami" "bastion_os_image" {
   most_recent = true
