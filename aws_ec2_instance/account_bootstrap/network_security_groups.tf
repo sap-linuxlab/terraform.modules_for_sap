e7b6a0178d50a69c00eab403a96ba4b5f58361d7
sean-freeman <sean.freeman@outlook.com>
2023-01-12 01:37:34 +0000

    fix: missing dns sg rule

--- aws_ec2_instance/account_bootstrap/network_security_groups.tf
+++ aws_ec2_instance/account_bootstrap/network_security_groups.tf
@@ -11,6 +11,25 @@ resource "aws_security_group" "vpc_sg" {
 
 }
 
+# Allow Outbound DNS Port 53 connection to IBM Cloud VPC DNS resolvers
+resource "aws_security_group_rule" "vpc_sg_rule_outbound_dns_tcp" {
+  security_group_id = aws_security_group.vpc_sg.id
+  type              = "egress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "tcp"
+  cidr_blocks       = ["${local.target_subnet_ip_range}"]
+}
+
+# Allow Outbound DNS Port 53 connection to IBM Cloud VPC DNS resolvers
+resource "aws_security_group_rule" "vpc_sg_rule_outbound_dns_udp" {
+  security_group_id = aws_security_group.vpc_sg.id
+  type              = "egress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "udp"
+  cidr_blocks       = ["${local.target_subnet_ip_range}"]
+}
 
 # Allow Outbound HTTP Port 80 connection to any (e.g. via NAT Gateway)
 resource "aws_security_group_rule" "vpc_sg_rule_outbound_http_80" {
