14112e4e266e80f1a64195fff834d85548c5ff1a
sean-freeman <sean.freeman@outlook.com>
2023-02-01 01:00:37 +0000

    fix: increase timeouts for slower connections

--- all/ansible_sap_bw4hana_install/exec_ansible_playbook.tf
+++ all/ansible_sap_bw4hana_install/exec_ansible_playbook.tf
@@ -29,7 +29,7 @@ resource "null_resource" "ansible_exec" {
   depends_on = [local_file.ansible_extravars, local_file.bastion_rsa, local_file.hosts_rsa]
   count = local.dry_run_boolean ? 0 : 1
 
-  # for ansible-playbook, use timeout set to 60 seconds to avoid error "Connection timed out during banner exchange"
+  # for ansible-playbook, use timeout set to 180 seconds to avoid error "Connection timed out during banner exchange"
   # for ansible-playbook, use debug with connection details -vvvv if errors occur
   provisioner "local-exec" {
     command = <<EOT
@@ -40,20 +40,30 @@ resource "null_resource" "ansible_exec" {
     export GIT_CONFIG_GLOBAL=/dev/null
     export GIT_CONFIG_SYSTEM=/dev/null
 
-    # Ansible Config - Default timeout for connection plugins to use. Equivilant to 'ansible-playbook --timeout 60' command
-    export ANSIBLE_TIMEOUT=90
+    # Documentation regarding SSH and Timeout configurations
+    # https://docs.ansible.com/ansible/latest/reference_appendices/config.html
+    # https://docs.ansible.com/ansible/latest/collections/ansible/builtin/ssh_connection.html
+
+    # Ansible Config - Default timeout for connection plugins to use. Equivilant to 'ansible-playbook --timeout 180' command, and creates SSH connection with '-o ConnectTimeout=180'.
+    export ANSIBLE_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection response from remote device
-    export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection retry to the local socket
-    export ANSIBLE_PERSISTENT_CONNECT_RETRY_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_CONNECT_RETRY_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection idle before closed
-    export ANSIBLE_PERSISTENT_CONNECT_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_CONNECT_TIMEOUT=180
 
     # Ansible Config - Forces color mode when run without a TTY
     export ANSIBLE_FORCE_COLOR=1
+
+    # Ansible Config - Arguments to pass to all SSH CLI tools. To ensure larger timeouts, overwrite the default "-C -o ControlMaster=auto -o ControlPersist=60s"
+    export ANSIBLE_SSH_ARGS="-C"
+
+    # Ansible Config - Common extra args for all SSH CLI tools. Leave blank, same as default.
+    export ANSIBLE_SSH_COMMON_ARGS=""
 
     ansible_2_9_version="$(ansible --version | awk 'NR==1{print $2}' | sed 's/]//g')"
     ansible_core_version="$(ansible --version | awk 'NR==1{print $3}' | sed 's/]//g')"
@@ -98,7 +108,7 @@ resource "null_resource" "ansible_exec" {
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
       elif [ $bastion_boolean == "false" ]; then
         ansible-playbook ${path.module}/ansible_playbook.yml \
         --extra-vars "@${path.root}/tmp/${var.module_var_hostname}/ansible_vars.yml" \
@@ -106,7 +116,7 @@ resource "null_resource" "ansible_exec" {
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
       fi
 EOF
       chmod +x ansible_exec.sh
@@ -119,7 +129,7 @@ EOF
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
       elif [ $bastion_boolean == "false" ]; then
         ansible-playbook ${path.module}/ansible_playbook.yml \
         --extra-vars "@${path.root}/tmp/${var.module_var_hostname}/ansible_vars.yml" \
@@ -127,7 +137,7 @@ EOF
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
       fi
     fi
 
--- all/ansible_sap_ecc_hana_install/exec_ansible_playbook.tf
+++ all/ansible_sap_ecc_hana_install/exec_ansible_playbook.tf
@@ -36,24 +36,30 @@ resource "null_resource" "ansible_exec" {
     # If Terraform Cloud/Enterprise, install Ansible to the Workspace Run container (Ubuntu)
     if [ ! -z "$TFC_RUN_ID" ] || [ ! -z "$TFE_RUN_ID" ] ; then echo 'Install Ansible to Terraform Cloud/Enterprise runtime' && python3 -m pip install ansible ; fi
 
-    # Git 2.32.0 and above - ignore the global Git config (e.g. ~/.gitconfig) and system Git config (e.g. /usr/local/etc/gitconfig)
-    export GIT_CONFIG_GLOBAL=/dev/null
-    export GIT_CONFIG_SYSTEM=/dev/null
+    # Documentation regarding SSH and Timeout configurations
+    # https://docs.ansible.com/ansible/latest/reference_appendices/config.html
+    # https://docs.ansible.com/ansible/latest/collections/ansible/builtin/ssh_connection.html
 
-    # Ansible Config - Default timeout for connection plugins to use. Equivilant to 'ansible-playbook --timeout 60' command
-    export ANSIBLE_TIMEOUT=90
+    # Ansible Config - Default timeout for connection plugins to use. Equivilant to 'ansible-playbook --timeout 180' command, and creates SSH connection with '-o ConnectTimeout=180'.
+    export ANSIBLE_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection response from remote device
-    export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_COMMAND_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection retry to the local socket
-    export ANSIBLE_PERSISTENT_CONNECT_RETRY_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_CONNECT_RETRY_TIMEOUT=180
 
     # Ansible Config - Timeout for persistent connection idle before closed
-    export ANSIBLE_PERSISTENT_CONNECT_TIMEOUT=120
+    export ANSIBLE_PERSISTENT_CONNECT_TIMEOUT=180
 
     # Ansible Config - Forces color mode when run without a TTY
     export ANSIBLE_FORCE_COLOR=1
+
+    # Ansible Config - Arguments to pass to all SSH CLI tools. To ensure larger timeouts, overwrite the default "-C -o ControlMaster=auto -o ControlPersist=60s"
+    export ANSIBLE_SSH_ARGS="-C"
+
+    # Ansible Config - Common extra args for all SSH CLI tools. Leave blank, same as default.
+    export ANSIBLE_SSH_COMMON_ARGS=""
 
     ansible_2_9_version="$(ansible --version | awk 'NR==1{print $2}' | sed 's/]//g')"
     ansible_core_version="$(ansible --version | awk 'NR==1{print $3}' | sed 's/]//g')"
@@ -98,7 +104,7 @@ resource "null_resource" "ansible_exec" {
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
       elif [ $bastion_boolean == "false" ]; then
         ansible-playbook ${path.module}/ansible_playbook.yml \
         --extra-vars "@${path.root}/tmp/${var.module_var_hostname}/ansible_vars.yml" \
@@ -106,7 +112,7 @@ resource "null_resource" "ansible_exec" {
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
       fi
 EOF
       chmod +x ansible_exec.sh
@@ -119,7 +125,7 @@ EOF
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no -o ProxyCommand='ssh -W %h:%p ${var.module_var_bastion_user}@${var.module_var_bastion_floating_ip} -p ${var.module_var_bastion_ssh_port} -i ${path.root}/tmp/${var.module_var_hostname}/bastion_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
       elif [ $bastion_boolean == "false" ]; then
         ansible-playbook ${path.module}/ansible_playbook.yml \
         --extra-vars "@${path.root}/tmp/${var.module_var_hostname}/ansible_vars.yml" \
@@ -127,7 +133,7 @@ EOF
         --user root \
         --inventory '${var.module_var_host_private_ip},' \
         --private-key '${path.root}/tmp/${var.module_var_hostname}/hosts_rsa' \
-        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=1800s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
+        --ssh-extra-args="-o ControlMaster=auto -o ControlPersist=3600s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ForwardX11=no"
       fi
     fi
 
