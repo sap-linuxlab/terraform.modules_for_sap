55e8d5b03d06ffb356e7d7a1fa110a13f9e46ebb
sean-freeman <sean.freeman@outlook.com>
2023-02-09 00:44:42 +0000

    fix: fqdn set

--- msazure_vm/host_provision/build_os_prepare.tf
+++ msazure_vm/host_provision/build_os_prepare.tf
@@ -24,21 +24,68 @@ resource "null_resource" "build_script_os_prepare" {
 # If this script fails to execute with error \r command not found, use below to convert CRLF to LF
 #sed -i "s/\r$//" FileName
 
+# Verify OS distribution
+export os_type=""
+export os_version=""
+os_info_id=$(grep ^ID= /etc/os-release | cut -d '=' -f2 | tr -d '\"')
+os_info_version=$(grep ^VERSION_ID= /etc/os-release | awk -F '=' '{print $2}' | tr -d '\"')
+if [ "$os_info_id" = "sles" ] || [ "$os_info_id" = "sles_sap" ] ; then os_type="sles" ; fi
+if [ "$os_info_id" = "rhel" ] ; then os_type="rhel" ; fi
+os_version=$os_info_version
+
 
 #############################################
 #	Set server hostname
 #############################################
 
+#echo "--- DEBUG ---"
+#cat /etc/hosts
+#cat /etc/resolv.conf
+#cat /etc/dhcp/dhclient.conf
+
+
+# Host and Domain vars
+ip_escaped=$(printf '%q\n' "${azurerm_linux_virtual_machine.host.private_ip_address}")
 set_hostname="${var.module_var_host_name}.${var.module_var_dns_root_domain_name}"
+set_hostname_only="${var.module_var_host_name}"
+set_domain_only="${var.module_var_dns_root_domain_name}"
+
+
+#### Begin hostname and domain change
+
+echo "Remove existing /etc/hosts record for host"
+sed -i "/$ip_escaped.*/d" /etc/hosts
+
+# Set Hostname in /etc/hosts manually
+echo -e "$ip_escaped\t$set_hostname\t$set_hostname_only" >> /etc/hosts
+
+# Set Hostname
 hostnamectl set-hostname $set_hostname
 systemctl restart systemd-hostnamed
 
-#echo "hostname --short  == $(hostname --short)"
-#echo "hostname --domain == $(hostname --domain)"
-#echo "hostname --fqdn   == $(hostname --fqdn)"
 
-#echo "Debug - show hosts file"
+if [ "$os_type" = "rhel" ]
+then
+    #### Reset network interface for hostname and domain to set
+    # Reload RHEL Network Manager
+    systemctl reload NetworkManager
+fi
+
+
+echo "--- Show hostname and domain details ---"
+echo "hostname --short  == $(hostname --short)"
+echo "hostname --domain == $(hostname --domain)"
+echo "hostname --fqdn   == $(hostname --fqdn)"
+echo "uname -n          == $(uname -n)"
+
+#echo "--- Show Ansible Fact for FQDN ---"
+#echo "Ansible Facts ansible_fqdn and ansible_domain uses python socket.getfqdn() == "
+#python3 -c "import socket; print(socket.getfqdn())"
+
+#echo "--- DEBUG ---"
 #cat /etc/hosts
+#cat /etc/resolv.conf
+#cat /etc/dhcp/dhclient.conf
 
 EOF
   }
