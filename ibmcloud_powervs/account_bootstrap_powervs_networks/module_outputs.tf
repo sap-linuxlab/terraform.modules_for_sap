3de0245a69e73999b7cd7a5bd5693e136072b6b9
sean-freeman <1815807+sean-freeman@users.noreply.github.com>
2023-10-30 20:57:07 +0000

    fix: split create of powervs workspace and networks

--- ibmcloud_powervs/account_bootstrap_powervs_networks/module_outputs.tf
+++ ibmcloud_powervs/account_bootstrap_powervs_networks/module_outputs.tf
@@ -1,8 +1,4 @@
 
-output "output_power_group_guid" {
-  value = ibm_resource_instance.power_group.guid
-}
-
 output "output_power_group_networks" {
   value = compact([
     ibm_pi_network.power_group_network_private.network_id,
---
+++ ibmcloud_powervs/account_bootstrap_powervs_networks/module_variables.tf
@@ -0,0 +1,10 @@
+
+variable "module_var_resource_prefix" {}
+
+variable "module_var_resource_group_id" {}
+
+variable "module_var_ibmcloud_power_zone" {}
+
+variable "module_var_ibmcloud_vpc_crn" {}
+
+variable "module_var_ibmcloud_powervs_workspace_guid" {}
